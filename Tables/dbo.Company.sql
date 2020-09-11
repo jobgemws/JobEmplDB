4f8eb9784b52d81acf750d6614291b0d8df675a3
Evgeniy Gribkov <37021038+jobgemws@users.noreply.github.com>
2020-09-11 23:03:58 +0300

    add column IsDeleted

--- Tables/dbo.Company.sql
+++ Tables/dbo.Company.sql
@@ -2,6 +2,7 @@
   [CompanyID] [int] IDENTITY,
   [CompanyName] [nvarchar](255) NOT NULL,
   [Description] [nvarchar](255) NOT NULL,
+  [IsDeleted] [bit] NOT NULL DEFAULT (0),
   CONSTRAINT [PK_Company_CompanyID] PRIMARY KEY CLUSTERED ([CompanyID])
 )
 ON [PRIMARY]
