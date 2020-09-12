9221eaae2424aad52cc5411b9b2396a8c0d16f9a
Evgeniy Gribkov <37021038+jobgemws@users.noreply.github.com>
2020-09-12 19:47:36 +0300

    add column Source

--- Tables/dbo.Company.sql
+++ Tables/dbo.Company.sql
@@ -3,6 +3,7 @@
   [CompanyName] [nvarchar](255) NOT NULL,
   [Description] [nvarchar](255) NOT NULL,
   [IsDeleted] [bit] NOT NULL DEFAULT (0),
+  [Source] [nvarchar](255) NULL,
   CONSTRAINT [PK_Company_CompanyID] PRIMARY KEY CLUSTERED ([CompanyID])
 )
 ON [PRIMARY]
